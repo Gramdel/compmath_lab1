1d4634999ff448dd6b1fa591ea8b79bd817b73b9
gramdel <legovec96@gmail.com>
2022-02-26 16:00:49 +0300

    basic input for matrix

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         int n;
-
         while (true) {
             System.out.print("Введите число неизвестных в системе (натуральное число <=20): ");
             String tmp = reader.nextLine();
@@ -13,21 +12,46 @@ public class Main {
                 n = Integer.parseInt(tmp);
                 if (n < 1 || n > 20) {
                     throw new NumberFormatException();
-                } else {
-                    break;
                 }
+                break;
             } catch (NumberFormatException e) {
-                if (!handleTypeErr("Требуется ввести натуральное число от 1 до 20 (включительно).")) {
+                if (!fixError("Требуется ввести натуральное число от 1 до 20 (включительно).")) {
                     System.out.println("Программа завершает работу.");
                     System.exit(-1);
                 }
             }
         }
+
+        double[][] a = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                while (true) {
+                    System.out.println("Введите значение элемента a_" + (i+1) + "_" + (j+1) + " матрицы коэффициентов\n(действительное число с точкой в качестве разделителя):");
+                    String tmp = reader.nextLine();
+                    try {
+                        a[i][j] = Double.parseDouble(tmp);
+                        break;
+                    } catch (NumberFormatException e) {
+                        if (!fixError("Требуется ввести действительное число с точкой (.) в качестве разделителя")) {
+                            System.out.println("Программа завершает работу.");
+                            System.exit(-1);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    private static boolean handleTypeErr(String msg) {
+    private static boolean fixError(String msg) {
         System.out.println("Некорректный ввод!\n" + msg);
-        System.out.print("Enter - повторить ввод, любая другая клавиша - выход: ");
+        System.out.print("Enter = повторить ввод, любая другая клавиша + Enter = выход: ");
         String check = reader.nextLine();
         return check.isEmpty();
     }
