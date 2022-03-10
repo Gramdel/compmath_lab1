e73824c41102da0ac2f5fc670c8aeb0751e8cff8
gramdel <legovec96@gmail.com>
2022-03-10 20:29:24 +0300

    Рефакторинг + задел на разные виды ввода

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.Math.abs;
@@ -7,9 +6,66 @@ public class Main {
     private static final Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Вас приветствует программа для демонстрации метода простых итераций!");
+        byte mode = chooseMode();
+
+        boolean isInteractive = false;
+        int n = inputN(isInteractive);
+        double[][] a = new double[n][n];
+        double[] b = new double[n];
+        inputSystem(isInteractive, n, a, b);
+        printSystem(n, a, b);
+
+        double eps = inputEps(isInteractive);
+        double[] x = new double[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = b[i] / a[i][i];
+        }
+        System.out.println(simpleIters(n, a, b, eps, x));
+    }
+
+    private static boolean handleError(String msg, boolean isInteractive) {
+        System.out.println("Некорректный ввод!\n" + msg);
+        if (isInteractive) {
+            System.out.println("Enter = повторить ввод, любая другая клавиша + Enter = выход: ");
+            String check = reader.nextLine();
+            return check.isEmpty();
+        }
+        return false;
+    }
+
+    private static void exit() {
+        System.out.println("Программа завершает работу.");
+        System.exit(-1);
+    }
+
+    private static byte chooseMode() {
+        byte mode;
+        while (true) {
+            System.out.println("Выберите режим работы программы (0 - ручной ввод, 1 - ввод из файла, 2 - автоматическая генерация):");
+            String tmp = reader.nextLine();
+            try {
+                mode = Byte.parseByte(tmp);
+                if (mode < 0 || mode > 2) {
+                    throw new NumberFormatException();
+                }
+                break;
+            } catch (NumberFormatException e) {
+                if (!handleError("Требуется ввести 0 для \"ручного ввода\", 1 - для \"ввода из файла\", 2 - для \"автоматической генерации\"", true)) {
+                    exit();
+                }
+            }
+        }
+        return mode;
+    }
+
+    private static int inputN(boolean isInteractive) {
         int n;
         while (true) {
-            System.out.println("Введите число неизвестных в системе (натуральное число <=20): ");
+            if (isInteractive) {
+                System.out.println("Введите число неизвестных в системе (натуральное число <=20): ");
+            }
+
             String tmp = reader.nextLine();
             try {
                 n = Integer.parseInt(tmp);
@@ -18,71 +74,72 @@ public class Main {
                 }
                 break;
             } catch (NumberFormatException e) {
-                if (!fixError("Требуется ввести натуральное число от 1 до 20 (включительно).")) {
-                    System.out.println("Программа завершает работу.");
-                    System.exit(0);
+                if (!handleError("Требуется ввести натуральное число от 1 до 20 (включительно).", isInteractive)) {
+                    exit();
                 }
             }
         }
+        return n;
+    }
 
-        double[][] a = new double[n][n];
-        double[] b = new double[n];
+    private static double inputEps(boolean isInteractive) {
+        double eps;
+        while (true) {
+            if (isInteractive) {
+                System.out.println("Введите точность (действительное число >0): ");
+            }
+
+            String tmp = reader.nextLine();
+            try {
+                eps = Double.parseDouble(tmp);
+                if (eps <= 0) {
+                    throw new NumberFormatException();
+                }
+                break;
+            } catch (NumberFormatException e) {
+                if (!handleError("Требуется ввести действительное число, большее 0.", isInteractive)) {
+                    exit();
+                }
+            }
+        }
+        return eps;
+    }
+
+    private static void inputSystem(boolean isInteractive, int n, double[][] a, double[] b) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 while (true) {
-                    System.out.println("Введите значение элемента a" + toLowerIndex(i + 1) + (char) 0x202F + toLowerIndex(j + 1) + " матрицы коэффициентов\n(действительное число с точкой в качестве разделителя):");
+                    if (isInteractive) {
+                        System.out.println("Введите значение элемента a" + toLowerIndex(i + 1) + toLowerIndex(j + 1) + " матрицы коэффициентов\n(действительное число с точкой в качестве разделителя):");
+                    }
+
                     String tmp = reader.nextLine();
                     try {
                         a[i][j] = Double.parseDouble(tmp);
                         break;
                     } catch (NumberFormatException e) {
-                        if (!fixError("Требуется ввести действительное число с точкой (.) в качестве разделителя")) {
-                            System.out.println("Программа завершает работу.");
-                            System.exit(0);
+                        if (!handleError("Требуется ввести действительное число с точкой (.) в качестве разделителя", isInteractive)) {
+                            exit();
                         }
                     }
                 }
             }
             while (true) {
-                System.out.println("Введите значение элемента b" + toLowerIndex(i + 1) + " столбца свободных членов\n(действительное число с точкой в качестве разделителя):");
+                if (isInteractive) {
+                    System.out.println("Введите значение элемента b" + toLowerIndex(i + 1) + " столбца свободных членов\n(действительное число с точкой в качестве разделителя):");
+                }
+
                 String tmp = reader.nextLine();
                 try {
                     b[i] = Double.parseDouble(tmp);
                     break;
                 } catch (NumberFormatException e) {
-                    if (!fixError("Требуется ввести действительное число с точкой (.) в качестве разделителя")) {
-                        System.out.println("Программа завершает работу.");
-                        System.exit(-1);
+                    if (!handleError("Требуется ввести действительное число с точкой (.) в качестве разделителя", isInteractive)) {
+                        exit();
                     }
                 }
             }
         }
-
-        System.out.println("Введена следующая система:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j != 0) {
-                    System.out.print(" + ");
-                }
-                System.out.print("(" + a[i][j] + ")·x" + toLowerIndex(j + 1));
-            }
-            System.out.println(" = " + b[i]);
-        }
-
-        double[] x = new double[n];
-        for (int i = 0; i < n; i++) {
-            x[i] = b[i] / a[i][i];
-        }
-
-        System.out.println(simpleIters(n, a, b, 0.0001, x));
-        System.out.println(Arrays.toString(x));
-    }
-
-    private static boolean fixError(String msg) {
-        System.out.println("Некорректный ввод!\n" + msg);
-        System.out.println("Enter = повторить ввод, любая другая клавиша + Enter = выход: ");
-        String check = reader.nextLine();
-        return check.isEmpty();
     }
 
     private static String toLowerIndex(int n) {
@@ -94,8 +151,22 @@ public class Main {
         return result.toString();
     }
 
-    private static int simpleIters(int n, double[][] a, double[] b, double eps, double[] x) {
+    private static void printSystem(int n, double[][] a, double[] b) {
+        System.out.println("Введена следующая система:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j != 0) {
+                    System.out.print(" + ");
+                }
+                System.out.print("(" + a[i][j] + ")·x" + toLowerIndex(j + 1));
+            }
+            System.out.println(" = " + b[i]);
+        }
+    }
+
+    private static Result simpleIters(int n, double[][] a, double[] b, double eps, double[] x) {
         double[] prevX = new double[n];
+        double[] delta = new double[n];
         double maxDelta;
         int count = 0;
 
@@ -111,13 +182,13 @@ public class Main {
                     }
                 }
 
-                double delta = abs(x[i] - prevX[i]);
-                if (delta > maxDelta) {
-                    maxDelta = delta;
+                delta[i] = abs(x[i] - prevX[i]);
+                if (delta[i] > maxDelta) {
+                    maxDelta = delta[i];
                 }
             }
         } while (maxDelta > eps);
 
-        return count;
+        return new Result(x, delta, count);
     }
 }
